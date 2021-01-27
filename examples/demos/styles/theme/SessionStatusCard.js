37749c904258bbede93d88904f1f9844e4bb53c9
fdrissi- <drissi.toubbali.fadel@gmail.com>
2021-01-27 17:15:43 +0100

    style: modify sessionCard style

--- examples/demos/styles/theme/SessionStatusCard.js
+++ examples/demos/styles/theme/SessionStatusCard.js
@@ -3,7 +3,7 @@ import textStyles from './textStyles';
 const SessionStatusCard = {
   parts: ['name', 'time', 'action', 'line'],
   baseStyle: {
-    height: '100px',
+    // height: '100px',
     borderRadius: '.25rem',
     name: {
       ...textStyles.buttonM,
@@ -19,7 +19,7 @@ const SessionStatusCard = {
     line: {
       position: 'absolute',
       left: '0%',
-      right: '99.5%',
+      right: '99.8%',
       top: '0%',
       bottom: '0%',
       borderRadius: '1rem 0rem 0 rem 1rem',
@@ -38,12 +38,6 @@ const SessionStatusCard = {
         whiteSpace: 'nowrap',
         width: '119px',
       },
-      time: {
-        display: 'none',
-      },
-      action: {
-        display: 'none',
-      },
     },
     md: {
       name: {
@@ -51,9 +45,6 @@ const SessionStatusCard = {
         overflow: 'hidden',
         whiteSpace: 'nowrap',
         width: '111px',
-      },
-      action: {
-        display: 'none',
       },
     },
     lg: {
@@ -67,9 +58,6 @@ const SessionStatusCard = {
         overflow: 'hidden',
         whiteSpace: 'nowrap',
       },
-      action: {
-        display: 'none',
-      },
     },
   },
   variants: {
@@ -77,16 +65,25 @@ const SessionStatusCard = {
       backgroundColor: 'primary.red.100',
       color: 'primary.red.300',
       borderColor: 'primary.red.500',
-      p: 2,
+      p: '8px',
       line: {
         backgroundColor: 'primary.red.500',
+      },
+    },
+    addSession: {
+      backgroundColor: 'primary.blue.100',
+      color: 'primary.blue.500',
+      borderColor: 'primary.blue.100',
+      p: '8px',
+      line: {
+        backgroundColor: 'primary.blue.300',
       },
     },
     next: {
       backgroundColor: 'primary.marine.100',
       color: 'primary.marine.700',
       borderColor: 'primary.marine.500',
-      p: 2,
+      p: '8px',
       line: {
         backgroundColor: 'primary.marine.500',
       },
@@ -95,7 +92,7 @@ const SessionStatusCard = {
       backgroundColor: 'primary.blue.100',
       color: 'primary.blue.500',
       borderColor: 'primary.blue.300',
-      p: 2,
+      p: '8px',
       line: {
         backgroundColor: 'primary.blue.300',
       },
@@ -104,7 +101,7 @@ const SessionStatusCard = {
       backgroundColor: 'secondary.alice.300',
       color: 'secondary.alice.700',
       borderColor: 'secondary.alice.700',
-      p: 2,
+      p: '8px',
       line: {
         backgroundColor: 'secondary.alice.700',
       },
