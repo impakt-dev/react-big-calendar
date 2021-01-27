5a71c1c901b24bad7293a8911837906be38feb8c
fdrissi- <drissi.toubbali.fadel@gmail.com>
2021-01-27 17:14:48 +0100

    chore: add event variants

--- examples/events.js
+++ examples/events.js
@@ -7,12 +7,14 @@ export default [
     allDay: true,
     start: new Date(2015, 3, 0),
     end: new Date(2015, 3, 1),
+    variant: 'upcoming',
   },
   {
     id: 1,
     title: 'Long Event',
     start: new Date(2015, 3, 7),
     end: new Date(2015, 3, 10),
+    variant: 'completed',
   },
 
   {
@@ -20,6 +22,7 @@ export default [
     title: 'DTS STARTS',
     start: new Date(2016, 2, 13, 0, 0, 0),
     end: new Date(2016, 2, 20, 0, 0, 0),
+    variant: 'next',
   },
 
   {
@@ -27,6 +30,7 @@ export default [
     title: 'DTS ENDS',
     start: new Date(2016, 10, 6, 0, 0, 0),
     end: new Date(2016, 10, 13, 0, 0, 0),
+    variant: 'canceled',
   },
 
   {
@@ -34,6 +38,7 @@ export default [
     title: 'Some Event',
     start: new Date(2015, 3, 9, 0, 0, 0),
     end: new Date(2015, 3, 10, 0, 0, 0),
+    variant: 'upcoming',
   },
   {
     id: 5,
@@ -41,6 +46,7 @@ export default [
     start: new Date(2015, 3, 11),
     end: new Date(2015, 3, 13),
     desc: 'Big conference for important people',
+    variant: 'next',
   },
   {
     id: 6,
@@ -48,6 +54,7 @@ export default [
     start: new Date(2015, 3, 12, 10, 30, 0, 0),
     end: new Date(2015, 3, 12, 12, 30, 0, 0),
     desc: 'Pre-meeting meeting, to prepare for the meeting',
+    variant: 'addSession',
   },
   {
     id: 7,
@@ -55,12 +62,14 @@ export default [
     start: new Date(2015, 3, 12, 12, 0, 0, 0),
     end: new Date(2015, 3, 12, 13, 0, 0, 0),
     desc: 'Power lunch',
+    variant: 'canceled',
   },
   {
     id: 8,
     title: 'Meeting',
     start: new Date(2015, 3, 12, 14, 0, 0, 0),
     end: new Date(2015, 3, 12, 15, 0, 0, 0),
+    variant: 'upcoming',
   },
   {
     id: 9,
@@ -68,76 +77,76 @@ export default [
     start: new Date(2015, 3, 12, 17, 0, 0, 0),
     end: new Date(2015, 3, 12, 17, 30, 0, 0),
     desc: 'Most important meal of the day',
+    variant: 'next',
   },
   {
     id: 10,
     title: 'Dinner',
     start: new Date(2015, 3, 12, 20, 0, 0, 0),
     end: new Date(2015, 3, 12, 21, 0, 0, 0),
+    variant: 'completed',
   },
   {
     id: 11,
     title: 'Birthday Party',
     start: new Date(2015, 3, 13, 7, 0, 0),
     end: new Date(2015, 3, 13, 10, 30, 0),
+    variant: 'canceled',
   },
   {
     id: 12,
     title: 'Late Night Event',
     start: new Date(2015, 3, 17, 19, 30, 0),
     end: new Date(2015, 3, 18, 2, 0, 0),
+    variant: 'addSession',
   },
   {
     id: 12.5,
     title: 'Late Same Night Event',
     start: new Date(2015, 3, 17, 19, 30, 0),
     end: new Date(2015, 3, 17, 23, 30, 0),
+    variant: 'upcoming',
   },
   {
     id: 13,
     title: 'Multi-day Event',
     start: new Date(2015, 3, 20, 19, 30, 0),
     end: new Date(2015, 3, 22, 2, 0, 0),
+    variant: 'next',
   },
   {
     id: 14,
-    title: 'Yoga Course 101 - Clean Your Mind from morning to night',
     start: new Date(new Date().setHours(new Date().getHours() - 10 * 48)),
     end: new Date(new Date().setHours(new Date().getHours() - 10 * 44)),
-    variant: 'test',
-    isLive: false,
-    isFinished: false,
+    title: 'Yoga Course 101 - Clean Your Mind from morning to night',
+    variant: 'completed',
   },
   {
     id: 140,
-    title: 'Yoga Course 101 - Clean Your Mind from night to morning',
-    start: new Date(new Date().setHours(new Date().getHours())),
-    end: new Date(new Date().setMinutes(new Date().getMinutes() + 180)),
-    isLive: true,
-    variant: 'upcoming'
+    title: 'Yoga Course 101 - Clean Your Mind from night to morninggg',
+    start: new Date(new Date().setHours(new Date().getHours() + 0.5 )),
+    end: new Date(new Date().setMinutes(new Date().getMinutes() + 1)),
+    variant: 'canceled'
   },
   {
     id: 141,
     title: 'Yoga Course 101 - Clean Your Mind from night to morning 2x',
-    start: new Date(new Date().setHours(new Date().getHours() + 8)),
-    end: new Date(new Date().setHours(new Date().getHours() + 9)),
-    isLive: false,
-    variant: 'canceled'
+    start: new Date(new Date().setHours(new Date().getHours() + 0.5)),
+    end: new Date(new Date().setHours(new Date().getHours() + 1)),
+    variant: 'next'
   },
   {
     id: 145,
     title: 'Yoga Course 101 - Clean Your Mind from night to morning x',
     start: new Date(new Date().setHours(new Date().getHours() + 7)),
     end: new Date(new Date().setHours(new Date().getHours() + 8)),
-    isLive: false,
     variant: 'completed'
   },
   {
     id: 147,
-    title: 'Yoga Course 101 - Clean Your Mind from night to morning x',
-    start: new Date(new Date().setHours(new Date().getHours() + 1)),
-    end: new Date(new Date().setHours(new Date().getHours() + 3)),
-    isLive: false,
+    title: 'Yoga Course 101 - Clean Your Mind from night to morning',
+    start: new Date(new Date().setHours(new Date().getHours() + 0.5)),
+    end: new Date(new Date().setHours(new Date().getHours() + 1)),
     variant: 'next'
   },
   { 
@@ -145,61 +154,69 @@ export default [
     title: 'Yoga Course 101 - Clean Your Mind from night to morning x',
     start: new Date(new Date().setHours(new Date().getHours() + 48)),
     end: new Date(new Date().setHours(new Date().getHours() + 49)),
-    isLive: false,
+    variant: 'completed'
   },
   { 
     id: 147,
     title: 'Yoga Course 101 - Clean Your Mind from night to morning x',
     start: new Date(new Date().setHours(new Date().getHours() + 96)),
     end: new Date(new Date().setHours(new Date().getHours() + 98)),
-    isLive: false,
+    variant: 'canceled',
   },
   {
     id: 16,
     title: 'Video Record',
     start: new Date(2015, 3, 14, 15, 30, 0),
     end: new Date(2015, 3, 14, 19, 0, 0),
+    variant: 'upcoming',
   },
   {
     id: 17,
     title: 'Dutch Song Producing',
     start: new Date(2015, 3, 14, 16, 30, 0),
     end: new Date(2015, 3, 14, 20, 0, 0),
+    variant: 'addSession',
   },
   {
     id: 18,
     title: 'Itaewon Halloween Meeting',
     start: new Date(2015, 3, 14, 16, 30, 0),
     end: new Date(2015, 3, 14, 17, 30, 0),
+    variant: 'completed',
   },
   {
     id: 19,
     title: 'Online Coding Test',
     start: new Date(2015, 3, 14, 17, 30, 0),
     end: new Date(2015, 3, 14, 20, 30, 0),
+    variant: 'canceled',
   },
   {
     id: 20,
     title: 'An overlapped Event',
     start: new Date(2015, 3, 14, 17, 0, 0),
     end: new Date(2015, 3, 14, 18, 30, 0),
+    variant: 'upcoming',
   },
   {
     id: 21,
     title: 'Phone Interview',
     start: new Date(2015, 3, 14, 17, 0, 0),
     end: new Date(2015, 3, 14, 18, 30, 0),
+    variant: 'next',
   },
   {
     id: 22,
     title: 'Cooking Class',
     start: new Date(2015, 3, 14, 17, 30, 0),
     end: new Date(2015, 3, 14, 19, 0, 0),
+    variant: 'addSession',
   },
   {
     id: 23,
     title: 'Go to the gym',
     start: new Date(2015, 3, 14, 18, 30, 0),
     end: new Date(2015, 3, 14, 20, 0, 0),
+    variant: 'next',
   },
 ]
