4610f33674794d60af3e72672aefd877446ebbb8
fdrissi- <drissi.toubbali.fadel@gmail.com>
2021-02-03 21:44:02 +0100

    style: set example calendar height

--- examples/demos/basic.js
+++ examples/demos/basic.js
@@ -2,7 +2,7 @@ import moment from 'moment'
 import React from 'react'
 import { Calendar, momentLocalizer, Views } from 'react-big-calendar'
 import events from '../events'
-import {  ChakraProvider, Box}  from "@chakra-ui/react"
+import {  ChakraProvider, Box, Flex}  from "@chakra-ui/react"
 import SessionStatusCard from './SessionStatusCard';
 import theme from './styles/theme/chakraTheme';
 
@@ -11,7 +11,8 @@ const allViews = Object.keys(Views).map(k => Views[k])
 const localizer = momentLocalizer(moment)
 const Basic = ({  }) => (
   <ChakraProvider theme={theme}>
-    <Calendar
+    <Flex mt={4} h="770px" borderRadius="lg">
+      <Calendar
       events={events}
       views={allViews}
       timeslots={2}
@@ -23,6 +24,8 @@ const Basic = ({  }) => (
         sessionCard: ({ title, startTime, endTime, variant, size, rating, isLive, isAvailable, onClick }) => <Box w="full" h="full"><SessionStatusCard sessionName={title} variant={variant} startTime={startTime} endTime={endTime} size={size} rating={rating} isLive={isLive} isAvailable={isAvailable} onClick={onClick} /></Box>
       }}
     />
+    </Flex>
+    
   </ChakraProvider>
 )
 
